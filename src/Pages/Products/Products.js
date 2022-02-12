e45741f15252345a2f529c3555e62939bbf32e29
Cécile HIRSCHAUER <cecile.hirschauer@suricats-consulting.com>
2022-02-12 17:48:53 +0100

    Create UI and cards

--- src/Pages/Products/Products.js
+++ src/Pages/Products/Products.js
@@ -1,7 +1,39 @@
-import React from 'react'
+import React from 'react';
+import './Products.css';
+import heart from "./heart.svg";
+import { Link } from 'react-router-dom';
+import inventory from '../../data/inventory';
 
 export default function Products() {
   return (
-    <div>Products</div>
+    <div className='container-products'>
+      {inventory.map(item => (
+        <Link 
+          to={{
+            pathname: `/produits/${item.title.replace(/\s+/g, '').trim()}`,
+          }}
+          key={item.id}
+          >
+          <div className="bloc-card">
+            <div className="product-card">
+              <div className="visual-aspect">
+                <img 
+                src={process.env.PUBLIC_URL + `/images/${item.img}.png`} 
+                alt="produit" 
+                className="img-product" />
+                <div className="like-container">
+                  <img src={heart} alt="icône j'aime" />
+                </div>
+              </div>
+              <div className="info">
+                <p>{item.title}</p>
+                <p>Prix : {item.price}€</p>
+              </div>
+            </div>
+            <div className="back-card"></div>
+          </div>
+        </Link>
+      ))}
+    </div>
   )
 }
