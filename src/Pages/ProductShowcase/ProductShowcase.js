fca4324bd1d45266c5a45710255657632e570ded
Cécile HIRSCHAUER <cecile.hirschauer@suricats-consulting.com>
2022-02-12 18:28:43 +0100

    Create product page

--- src/Pages/ProductShowcase/ProductShowcase.js
+++ src/Pages/ProductShowcase/ProductShowcase.js
@@ -1,7 +1,44 @@
-import React from 'react'
+import React, {useState, useEffect, useRef} from 'react';
+import { useParams } from 'react-router-dom';
+import './ProductShowcase.css';
+import inventory from '../../data/inventory';
+import { useDispatch } from 'react-redux';
 
 export default function ProductShowcase() {
+
+  const [nbMugs, setNbMugs] = useState(1);
+
+  const {id} = useParams();
+  console.log(id);
+
+  const productClicked = inventory.findIndex(obj => obj.title.replace(/\s+/g, '').trim() === id);
+  
+  console.log(productClicked);
+
+  const updateMugs = e => {
+    setNbMugs(Number(e.target.value))
+  }
+
   return (
-    <div>ProductShowcase</div>
+    <div className='showcase'>
+      <div className="container-img-showcase">
+        <img 
+          src={process.env.PUBLIC_URL + `/images/${inventory[productClicked].img}.png`} 
+          alt="" 
+          className="img-showcase" />
+      </div>
+      <div className="product-infos">
+        <h2>{inventory[productClicked].title}</h2>
+        <p>Prix : {inventory[productClicked].price}€</p>
+        <form>
+          <label htmlFor="quantity">Quantité</label>
+          <input 
+            type="number" 
+            id="quantity" 
+            value={nbMugs}
+            onChange={updateMugs}/>
+        </form>
+      </div>
+    </div>
   )
 }
